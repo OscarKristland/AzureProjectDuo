8c99399e9e05d7d7e7ad53427c160bbe60a3eb6e
OscarKristland <113332057+OscarKristland@users.noreply.github.com>
2024-01-23 11:12:40 +0100

    Add project files.

---
+++ AzureProjectDuo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureProjectDuo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
